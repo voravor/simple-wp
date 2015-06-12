e160187587482f7e533fe3188cacd77cec2143d7
voravor <voravor@gmail.com>
2015-06-12 00:50:37 -0400

    add is_login_page util

--- simple-wp.php
+++ simple-wp.php
@@ -63,4 +63,11 @@ if(!function_exists('jsonp_decode')) {
         
     }
 }
+
+if(!function_exists('is_login_page')) {
+
+	function is_login_page() {
+    	return in_array($GLOBALS['pagenow'], array('wp-login.php', 'wp-register.php'));
+	}
+}
     
